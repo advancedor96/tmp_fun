3e469fe0e82005c30cac6009cbd7890e286fdfde
advancedor96 <advancedor96@gmail.com>
2022-02-22 15:15:40 +0800

    fix bug 12

--- src/views/OrderPage2.vue
+++ src/views/OrderPage2.vue
@@ -1,9 +1,6 @@
 <template>
 <v-container>
   <div class="text-h4">最後一步！</div>
-<div v-if="!$vuetify.breakpoint.xsOnly" class="d-flex justify-center">
-  <img width="250" height="250" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPoAAAD6AQAAAACgl2eQAAABvUlEQVR4Xu2Y3W3DMAyECWiAjOTVNZIHEMDej5o6QZG+9gATTiLTXx+upI5Kqj/HrPfMW9yA4wYcN+AQMAvxmEOJVceqWkz5WQyA13pMXPXAosd54N35IIC6kJfGZhhOBGafxMQEA+MsXMyTzANaLWd1fR7F9tv5IMDbf12v3/zhfwM7kPHGkZvtSALkWqUCsesgUO2XBUAdHQybRTJpAvSBLIA7XZ0mvcovOXMUwM/BljuwADPkyS/VDAD41DJLFwGU7zyiAO53l2bxuIWFTOxnoEQA02Od1UGlAGOmoHCX/0MKgA9uGXgXcpz17WQOwC2zdEpZOpno0Lv/KgjgNERpeMpS73F9eETmAJMy2XLstKGuY73qKTMCkHEJG2w5rCVckQOg02RfPJa4avtpFMDS+HbQzbB3wO9kDsAyOV+yL5tAGrAHOl3LY70JsHBJwHS+9TUQkr81zijAQawp2YuZBlCU6oXG4+3yfHwZKAEAXtwvGvFNK3uXGQHofAKApywrhe5WpAFTG5969T49VtIA5TnfaWLDv79FAS11i/ZFT3a0Igjgvb/PMigTwq/FSgA+xQ04bsBxA46/gS/xzQSDURzQNAAAAABJRU5ErkJggg==" alt="">
-</div>
  <br />
 <div class="text-body" >
 你報名的日期有：<br />
@@ -22,9 +19,17 @@
 用<span style="color: #4CAF50; font-weight: 800;">LINE 回傳</span>給我們，<br />
 以便確認能夠聯繫到您，<br /><br />
 
-⬇️ FUN星球官方Line連結<br />
-<a href="https://lin.ee/lpYPloz" target="_blank">https://lin.ee/lpYPloz</a>
-<br /><br />
+<div v-if="$vuetify.breakpoint.xsOnly">
+  ⬇️ FUN星球官方Line連結<br />
+  <a href="https://lin.ee/lpYPloz" target="_blank">https://lin.ee/lpYPloz</a>
+  <br /><br />
+</div>
+<div v-else>
+  <div>Line連結：<a href="https://lin.ee/lpYPloz" target="_blank">https://lin.ee/lpYPloz</a></div>
+  <div>或掉QR碼加入。</div>
+  <img style="margin-left: -35px;" width="350" height="350" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAPoAAAD6AQAAAACgl2eQAAABvUlEQVR4Xu2Y3W3DMAyECWiAjOTVNZIHEMDej5o6QZG+9gATTiLTXx+upI5Kqj/HrPfMW9yA4wYcN+AQMAvxmEOJVceqWkz5WQyA13pMXPXAosd54N35IIC6kJfGZhhOBGafxMQEA+MsXMyTzANaLWd1fR7F9tv5IMDbf12v3/zhfwM7kPHGkZvtSALkWqUCsesgUO2XBUAdHQybRTJpAvSBLIA7XZ0mvcovOXMUwM/BljuwADPkyS/VDAD41DJLFwGU7zyiAO53l2bxuIWFTOxnoEQA02Od1UGlAGOmoHCX/0MKgA9uGXgXcpz17WQOwC2zdEpZOpno0Lv/KgjgNERpeMpS73F9eETmAJMy2XLstKGuY73qKTMCkHEJG2w5rCVckQOg02RfPJa4avtpFMDS+HbQzbB3wO9kDsAyOV+yL5tAGrAHOl3LY70JsHBJwHS+9TUQkr81zijAQawp2YuZBlCU6oXG4+3yfHwZKAEAXtwvGvFNK3uXGQHofAKApywrhe5WpAFTG5969T49VtIA5TnfaWLDv79FAS11i/ZFT3a0Igjgvb/PMigTwq/FSgA+xQ04bsBxA46/gS/xzQSDURzQNAAAAABJRU5ErkJggg==" alt="">
+</div>
+
 若電話與Line都<span style="border-bottom: 2px solid red;">未能聯繫，<br />
 會刪除您該次的報名</span>，<br />
 後續的異動與提醒會優先透過Line進行通知，<br />
