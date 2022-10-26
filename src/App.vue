579848c73e9ce6e694677196d61eada4ed0f6d85
Ding <advancedor96@gmail.com>
2022-10-26 12:43:45 +0800

    加入footer

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,16 @@
       <v-btn icon v-if="$route.name !== 'OrderList'"  @click="$router.push('/OrderList')">
         <v-icon >mdi-arrow-left-bold</v-icon>
       </v-btn>
-      <v-toolbar-title>(小哈家)Fun星球報名系統</v-toolbar-title>
+      <v-toolbar-title>Fun星球報名系統</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
       <router-view/>
+      <v-footer padless height="20" class="mt-9">
+        <div class="text-center" style="width: 100%; color: #858585;font-size:12px;">
+          Actived in 2022/04, made by ding.
+        </div>
+      </v-footer>
     </v-main>
   </v-app>
 </template>
