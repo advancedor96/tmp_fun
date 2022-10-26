9078bad68ffbe96ceaa35c63c7bd4cbe14789d14
Ding <advancedor96@gmail.com>
2022-10-26 09:55:09 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
-
     <v-app-bar app color="deep-purple accent-4" dark>
       <v-btn icon v-if="$route.name !== 'OrderList'"  @click="$router.push('/OrderList')">
         <v-icon >mdi-arrow-left-bold</v-icon>
       </v-btn>
-      <!-- <v-app-bar-nav-icon @click="$router.push('/OrderList')"></v-app-bar-nav-icon> -->
       <v-toolbar-title>(小哈家)Fun星球報名系統</v-toolbar-title>
     </v-app-bar>
 
