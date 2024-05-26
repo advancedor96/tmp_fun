19a063b4283f9960f94205e361d68d480fb9171f
Ding <advancedor96@gmail.com>
2024-05-26 17:11:48 +0200

    fix bug

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -63,6 +63,7 @@ export default {
         this.isLoading = true
 
         const res = await axios.get(`/clientList/${this.dayjsObj.format('YYYY')}/${this.dayjsObj.format('MM')}`)
+        this.sList = res.data
         // this.sList = res.data.filter(e => e.event_type === 'session')
         console.log('來了：',this.sList);
         
@@ -80,6 +81,7 @@ export default {
         const prevMonthDayjsObj = this.dayjsObj.subtract(1, 'month')
 
         const res = await axios.get(`/clientList/${prevMonthDayjsObj.format('YYYY')}/${prevMonthDayjsObj.format('MM')}`)
+        this.sList = res.data
         // this.sList = res.data.filter(e => e.event_type === 'session')
         this.showMonth = prevMonthDayjsObj.format('M')
       } catch (err) {
@@ -95,6 +97,7 @@ export default {
         const nextMonthDayjsObj = this.dayjsObj.add(1, 'month')
 
         const res = await axios.get(`/clientList/${nextMonthDayjsObj.format('YYYY')}/${nextMonthDayjsObj.format('MM')}`)
+        this.sList = res.data
         // this.sList = res.data.filter(e => e.event_type === 'session')
         this.showMonth = nextMonthDayjsObj.format('M')
       } catch (err) {
